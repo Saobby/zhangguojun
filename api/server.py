530edf6b7e1ec7cd9ee78598b971f8e123364617
Saobby <101960076+Saobby@users.noreply.github.com>
2022-10-13 23:38:05 +0800

    Update server.py

--- api/server.py
+++ api/server.py
@@ -4,24 +4,18 @@ import json
 app = Flask(__name__)
 
 
-def check_ans(input_val):
-    inp = input_val
-    ans = []
-    for i in range(4):
-        a, b, c, d = inp[0], inp[1], inp[2], inp[3]
-        x1 = -a+b+c-d
-        x2 = -a-b+c+d
-        x3 = -a+b+c-d
-        x4 = -a+b-c+d
-        x5 = -a+b-c+d
-        x6 = -a-b+c+d
-        an = [x1, x2, x3, x4, x5, x6]
-        for j in an:
-            if j < 0 and j + a >= 0:
-                ans.append((i, j))
-        inp.append(inp.pop(0))
-    return ans
-
+def is_fail(i):
+    j = {}
+    for a in i:
+        if a in j:
+            j[a] += 1
+        else:
+            j[a] = 1
+    for k, v in j.items():
+        if v != 2:
+            return False
+    return True
+    
 
 @app.route("/api/act", methods=["get", "post"])
 def act():
@@ -44,7 +38,15 @@ def act():
     for a in ans:
         now = inp.copy()
         now[a[0]] += a[1]
-        if not check_ans(now):
+        ca = check_ans(now)
+        fail = False
+        for b in ca:
+            n = now.copy()
+            n[b[0]] += b[1]
+            if is_fail(n):
+                fail = True
+                break
+        if not fail:
             final_ans.append(a)
         else:
             f.append(check_ans(now))
@@ -53,4 +55,4 @@ def act():
         ret_ = inp.copy()
         ret_[a[0]] += a[1]
         ret.append(ret_)
-    return json.dumps(ret)+json.dumps(f)
+    return json.dumps(ret)+json.dumps(f)+json.dumps(ans)
